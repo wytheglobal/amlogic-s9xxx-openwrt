4aa446225dd2b1e405bcf5270621d6932c650108
tomwang <tomwang@didiglobal.com>
2022-03-15 17:35:20 +0800

    feat: add ssr+

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -34,6 +34,9 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
+# add ssr+
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
 # Add p7zip
 svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 
